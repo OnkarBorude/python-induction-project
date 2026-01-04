415fa334487e6c7a92911dcc919daf9b5f8e4c92
OnkarBorude <onkarb749@gmail.com>
2026-01-04 09:27:29 +0530

    add manoj and 1000

--- index.py
+++ index.py
@@ -4,4 +4,5 @@ Docstring for index
 print("onkar")
 print("Tankya","is intalligence")
 print("Tankya","is a smart boy")
-print("Virat number is 18")
+print("Virat number is 1000")
+print("my name is manoj")
