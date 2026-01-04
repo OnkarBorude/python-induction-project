9dc8a5fad8f4241db544845f33944eff91a1d3d8
OnkarBorude <onkarb749@gmail.com>
2026-01-04 09:36:04 +0530

    jrgnn

--- index.py
+++ index.py
@@ -4,5 +4,4 @@ Docstring for index
 print("onkar")
 print("Tankya","is intalligence")
 print("Tankya","is a smart boy")
-print("Virat number is 1000")
-print("my name is manoj")
+print("rohit number is 45")
